bba496a938622831a9de1885cc805e5711c827a6
carlos <crlos.rjas.2001@gmail.com>
2025-06-04 13:23:34 -0500

    04/06/2025

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,14 @@ import { AmigosModule } from './amigos/amigos.module';
       password: "DaPwSFXTdtybrPPJjMSqFmLGvKhXAoBM",
       database: "railway",
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
+       retryAttempts: 10,                        // Reintenta 10 veces si falla
+    retryDelay: 3000,                         // Espera 3 segundos entre intentos
+    extra: {
+      connectionLimit: 2,                     // Baja cantidad si usas Alwaysdata gratuito
+      keepAlive: true,
+    },
+
     }),
     AuthModule,
     MessagesModule,
